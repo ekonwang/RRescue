9c92c41695ab28e25d61b4ab92a233c5be94b454
root <g1547246193@gmail.com>
2023-06-29 13:26:08 +0000

    update run script

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash 
 # for innitializing new environment
+conda create -n rrhf python=3.8
+conda activate rrhf
 pip install torch==1.13.0+cu116 torchvision==0.14.0+cu116 torchaudio==0.13.0 --extra-index-url https://download.pytorch.org/whl/cu116
 source /etc/network_turbo
 wget https://github.com/huggingface/transformers/archive/refs/heads/main.zip
